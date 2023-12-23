55a9663c4aabea973306f4abac3a3113cb51ac40
Mohankrishnakatta <kattamohankrishna2001@gmail.com>
2023-12-23 17:26:18 +0530

    infeasible model

---
+++ py/q3.py
@@ -0,0 +1,14 @@
+from pyomo.environ import *
+
+model = m = ConcreteModel()
+
+m.x1 = Var(domain = Integers )
+m.x2 = Var(domain = Integers )
+
+
+m.constraint1 = Constraint(expr=(m.x1 >= 0))
+m.constraint2 = Constraint(expr=(m.x1 >= 0))
+m.constraint3 = Constraint(expr=(m.x1 + m.x2 <= -1))
+m.constraint4 = Constraint(expr=(2*m.x1 + 2*m.x2 >= 2))
+
+m.obj = Objective(sense=minimize, expr= (m.x1**2 + m.x2**2))
