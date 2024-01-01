9a3b65b2c26bd09012dcc99a7ff8c747c1a33114
Ashutosh Mahajan <amahajan@iitb.ac.in>
2024-01-01 06:17:26 +0530

    minor change to avoid duplicate lines

--- py/q3.py
+++ py/q3.py
@@ -7,7 +7,7 @@ m.x2 = Var(domain = Integers )
 
 
 m.constraint1 = Constraint(expr=(m.x1 >= 0))
-m.constraint2 = Constraint(expr=(m.x1 >= 0))
+m.constraint2 = Constraint(expr=(m.x2 >= 0))
 m.constraint3 = Constraint(expr=(m.x1 + m.x2 <= -1))
 m.constraint4 = Constraint(expr=(2*m.x1 + 2*m.x2 >= 2))
 
